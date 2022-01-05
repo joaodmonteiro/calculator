97f2d77e585e166513d6f998c621c22a668cc161
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-01-05 13:48:57 +0000

    Remove multiple points, 2 operators next to each other and round result to 8 decimal places

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(a, b) {
-    return +a + +b;
+    return Math.round((+a + +b + Number.EPSILON) * 100000000) / 100000000;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return Math.round((a - b + Number.EPSILON) * 100000000) / 100000000;
 }
 
 function multiply(a, b) {
-    return a * b;
+    return Math.round((a * b + Number.EPSILON) * 100000000) / 100000000;
 }
 
 function divide(a, b) {
-    return a/b;
+    return Math.round((a / b + Number.EPSILON) * 100000000) / 100000000;
 }
 
 function operate(operator, a, b) {
@@ -42,6 +42,7 @@ let operator;
 
 keys.forEach(key => {
     key.addEventListener('click', () => {
+
         if(key.textContent == 'Clear')
         {
             a = '';
@@ -80,9 +81,14 @@ keys.forEach(key => {
         {
             if(key.classList.contains('number'))
             {
-                screen2.textContent += key.textContent;
+                if(key.id === 'point'){
+                    if(screen2.textContent.charAt(screen2.textContent.length - 1) != '.')
+                        screen2.textContent += key.textContent;
+                }  
+                else
+                    screen2.textContent += key.textContent;
             }
-            else if(key.classList.contains('operator') && screen2.textContent)
+            else if(key.classList.contains('operator') && screen2.textContent && key.textContent != '=')
             {
                 a = screen2.textContent;
                 operator = key.textContent;
@@ -95,71 +101,86 @@ keys.forEach(key => {
             {
                 if(key.classList.contains('number'))
                 {
-                    screen2.textContent += key.textContent;
+                    if(key.id === 'point'){
+                        if(screen2.textContent.charAt(screen2.textContent.length - 1) != '.')
+                            screen2.textContent += key.textContent;
+                    }  
+                    else
+                        screen2.textContent += key.textContent;
                 }
-                else if(key.classList.contains('operator'))
+                else if(key.classList.contains('operator') && screen2.textContent)
                 {
                     b = screen2.textContent;
                     screen1.textContent += screen2.textContent;
                     screen2.textContent = '';
-                    if(key.id == 'equals')
-                    {
-                        switch(operator) {
-                            case '+':
-                                result = add(a, b);
-                                screen1.textContent += '=';
-                                screen2.textContent = result;
-                                break;
-                            case '-':
-                                result = subtract(a, b);
-                                screen1.textContent += '=';
-                                screen2.textContent = result;
-                                break;
-                            case 'X':
-                                result = multiply(a, b);
-                                screen1.textContent += '=';
-                                screen2.textContent = result;
-                                break;
-                            case '/':
-                                result = divide(a, b);
-                                screen1.textContent += '=';
-                                screen2.textContent = result;
-                                break;
-                        }   
-                    }
-                    else
-                    {
-                        switch(operator) {
-                            case '+':
-                                screen1.textContent = add(a, b);
-                                a = screen1.textContent;
-                                b = '';
-                                screen1.textContent += key.textContent;
-                                operator = key.textContent;
-                                break;
-                            case '-':
-                                screen1.textContent = subtract(a, b);
-                                a = screen1.textContent;
-                                b = '';
-                                screen1.textContent += key.textContent;
-                                operator = key.textContent;
-                                break;
-                            case 'X':
-                                screen1.textContent = multiply(a, b);
-                                a = screen1.textContent;
-                                b = '';
-                                screen1.textContent += key.textContent;
-                                operator = key.textContent;
-                                break;
-                            case '/':
-                                screen1.textContent = divide(a, b);
-                                a = screen1.textContent;
-                                b = '';
-                                screen1.textContent += key.textContent;
-                                operator = key.textContent;
-                                break;
+
+                    if(operator){
+                        // Calculate result
+                        if(key.id == 'equals')
+                        {
+                            switch(operator) {
+                                case '+':
+                                    result = add(a, b);
+                                    screen1.textContent += '=';
+                                    screen2.textContent = result;
+                                    break;
+                                case '-':
+                                    result = subtract(a, b);
+                                    screen1.textContent += '=';
+                                    screen2.textContent = result;
+                                    break;
+                                case 'X':
+                                    result = multiply(a, b);
+                                    screen1.textContent += '=';
+                                    screen2.textContent = result;
+                                    break;
+                                case '/':
+                                    result = divide(a, b);
+                                    screen1.textContent += '=';
+                                    screen2.textContent = result;
+                                    break;
+                            }   
+                        }
+                        else
+                        {
+                            switch(operator) {
+                                case '+':
+                                    screen1.textContent = add(a, b);
+                                    a = screen1.textContent;
+                                    b = '';
+                                    screen1.textContent += key.textContent;
+                                    operator = key.textContent;
+                                    break;
+                                case '-':
+                                    screen1.textContent = subtract(a, b);
+                                    a = screen1.textContent;
+                                    b = '';
+                                    screen1.textContent += key.textContent;
+                                    operator = key.textContent;
+                                    break;
+                                case 'X':
+                                    screen1.textContent = multiply(a, b);
+                                    a = screen1.textContent;
+                                    b = '';
+                                    screen1.textContent += key.textContent;
+                                    operator = key.textContent;
+                                    break;
+                                case '/':
+                                    screen1.textContent = divide(a, b);
+                                    a = screen1.textContent;
+                                    b = '';
+                                    screen1.textContent += key.textContent;
+                                    operator = key.textContent;
+                                    break;
+                            }
                         }
                     }
+                }
+                else if(key.classList.contains('operator') && !screen2.textContent && key.textContent != '=')
+                {
+                    screen1.textContent = screen1.textContent.slice(0, - 1);
+                    operator = key.textContent;
+                    screen1.textContent += operator;
                 }
             }
             // else
